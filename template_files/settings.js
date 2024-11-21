46f180a5f7eaf6d938c0122e7d802f87439df1b1
Byron Bingham <bybingham@gmail.com>
2024-11-20 19:17:38 -0700

    save

---
+++ template_files/settings.js
@@ -0,0 +1,11 @@
+/**
+ * Settings file for the web UI. Fill out this file before deploying a new web UI
+ */
+
+const db_table_name = '[table name]';
+const default_images_per_page=50;
+const webapp_name = '[webapp name]';
+const webapp_long_name = '[full webapp name]';
+const server_addr = '[webapp server address]:8080';
+const api_addr = '[webapp server address]:38001';
+const thumb_height = 200;
