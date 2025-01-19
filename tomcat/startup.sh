c053ffae086ed58e64f5fc54fe6d1b28575a4be0
Byron Bingham <bybingham@gmail.com>
2025-01-18 21:14:14 -0700

    fix?

--- tomcat/startup.sh
+++ tomcat/startup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ -v WEBAPP_NAME ]; then
+if [ -n WEBAPP_NAME ]; then
     mv /usr/local/tomcat/webapps/mediadb_gui/ /usr/local/tomcat/webapps/${WEBAPP_NAME}
     echo "Changed webapp name to '${WEBAPP_NAME}'"
 else    
